292ef90342841f31b7f51bbe3140eb8794d7fa78
Soumyajit Bhattacharjee <89545023+Jonti10@users.noreply.github.com>
2021-10-30 21:53:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import "./App.css";
+
+function App() {
+  const [user, setUsers] = useState()
+
+  const getuserdata = async () => {
+    const response = await fetch("https://reqres.in/api/users?page=1");
+    const data = await response.json();
+    setUsers(data.data);
+  }
+  return (
+    <div>
+      <h1 className="header">LetsGrowMore</h1>
+      <h1 className="header">Task 2:</h1>
+      <div className="btn">
+        <button onClick={getuserdata}>Connect Users</button>
+      </div>
+      {
+        <div className="content">
+          {user?.map((res, id) => {
+            return (
+              <div className="box" key={id}>
+                <img src={res.avatar} alt="userimage" />
+                <div className="id">ID:{res.id}</div>
+                <div className="name">{res.first_name} {res.last_name}</div>
+                <div className="email"><a href="#email"> {res.email}</a></div>
+              </div>
+            );
+          })}
+        </div>
+      }
+    </div>
+  );
+}
+export default App;
